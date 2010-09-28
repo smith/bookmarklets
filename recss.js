cefc85078588f1e831512d98e56e28df8dfc6c6c
Nathan L Smith <nlloyds@gmail.com>
2010-09-28 08:44:51 -0500

    added recss

---
+++ recss.js
@@ -0,0 +1,2 @@
+// See http://david.dojotoolkit.org/recss.html
+void(function(){var i,a,s;a=document.getElementsByTagName('link');for(i=0;i<a.length;i++){s=a[i];if(s.rel.toLowerCase().indexOf('stylesheet')>=0&&s.href) {var h=s.href.replace(/(&|%5C?)forceReload=\d+/,'');s.href=h+(h.indexOf('?')>=0?'&':'?')+'forceReload='+(new Date().valueOf())}}})();
