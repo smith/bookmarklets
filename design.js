cb96e0cc72eefc5a51a5986357f9b5cdac9992ad
Nathan L Smith <nlloyds@gmail.com>
2010-09-28 08:42:21 -0500

    added design

---
+++ design.js
@@ -0,0 +1,2 @@
+// See http://www.sprymedia.co.uk/article/Design
+function fnStartDesign(sUrl) {var nScript = document.createElement('script');nScript.setAttribute('language','JavaScript');nScript.setAttribute('src',sUrl);document.body.appendChild(nScript);}fnStartDesign('http://www.sprymedia.co.uk/design/design/media/js/design-loader.js')')'')')}" ')')'')')}
